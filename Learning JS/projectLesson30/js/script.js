e1ee1f1321365995fa8708a4cb0c5adde1df6900
Ilya <helix223@narod.ru>
2022-02-13 21:01:39 +0300

    update

--- Learning JS/projectLesson30/js/script.js
+++ Learning JS/projectLesson30/js/script.js
@@ -1,44 +1,103 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+// 1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+// новый фильм добавляется в список. Страница не должна перезагружаться.
+// Новый фильм должен добавляться в movieDB.movies.
+// Для получения доступа к значению input - обращаемся к нему как input.value;
+// P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+// 2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+/
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+// 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+// 4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+// "Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-// 1
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    // 1
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    promoBG = document.querySelector('.promo__bg'),
-    promGenr = promoBG.querySelector('.promo__genre'),
-    movieList = document.getElementsByClassName('promo__interactive-item');
+    const adv = document.querySelectorAll('.promo__adv img'),
+        promoBG = document.querySelector('.promo__bg'),
+        promGenr = promoBG.querySelector('.promo__genre'),
+        movieList = document.getElementsByClassName('promo__interactive-item'),
+        addMov = document.querySelector(".add"),
 
-adv.forEach(item => {
-    item.remove();
+        addMovText = addMov.querySelector('.adding__input'),
+        btn = document.getElementsByTagName('button'),
+        loved = document.querySelector('input[type=checkbox]');
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        promGenr.textContent = "ДРАМА";
+        promoBG.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+    function sortMov() {
+        let movieList = document.getElementsByClassName('promo__interactive-item');
+        movieDB.movies.sort();
+        for (let i = 0; i < movieList.length; i++) {
+            movieList[i].innerHTML = `№${i+1} ${movieDB.movies[i]} <div class="delete"></div>`;
+        }
+    }
+
+    makeChanges();
+    sortMov();
+    deleteAdv(adv);
+
+    const newMovie = (event) => {
+        event.preventDefault();
+        let a = addMovText.value;
+        if (a.length > 21) {
+            a = a.slice(0, 21) + '...';
+        }
+        if (a.length > 3) {
+            movieDB.movies.push(a);
+
+            const addLi = document.createElement('li');
+            addLi.innerHTML = `<li class="promo__interactive-item"> ${a} <div class = "delete"> </div></li>`;
+            movieList[movieList.length - 1].after(addLi);
+            sortMov();
+            addMovText.value = 'Добавлено!';
+            setTimeout(() => {
+                addMovText.parentNode.reset();
+            }, 1200);
+
+            if (loved.checked) {
+                console.log("Добавляем любимый фильм");
+            }
+        }
+    }
+
+    btn[0].addEventListener('click', newMovie);
+    const deleteMov = document.querySelectorAll('.delete');
+    console.log(deleteMov);
+
+    const deleteElement = (event) => {
+        event.currentTarget.parentNode.remove();
+    }
+
+    deleteMov.forEach(bin => {
+        bin.addEventListener('click', deleteElement)
+    });
+
+
 });
-
-promGenr.textContent = "ДРАМА";
-
-promoBG.style.backgroundImage = "url('img/bg.jpg')";
-
-movieDB.movies.sort();
-for (let i = 0; i < movieList.length; i++) {
-    movieList[i].textContent = `№${i+1} ${movieDB.movies[i]}`;
-}
--- Learning JS/script.js
+++ Learning JS/script.js
@@ -1,3 +1,4 @@
+'use strict';
 // const fruits = "Some fruits";
 // console.log(fruits.indexOf("fruits")); //5
 
@@ -116,45 +117,309 @@
 //     internet = [...video, ...blogs];
 //     console.log(internet);
 
-// Урок 23, ООП 
+// // Урок 23, ООП 
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log('Hello!');
-    }
-};
-
-const john = Object.create(soldier);
-
-// const john = {
-//     health: 100
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function () {
+//         console.log('Hello!');
+//     }
 // };
 
-// john.__proto__ = soldier; <-- устаревшая форма
+// const john = Object.create(soldier);
 
-// Object.setPrototypeOf(john, soldier);
+// // const john = {
+// //     health: 100
+// // };
 
-// console.log(john.armor);
-john.sayHello()
+// // john.__proto__ = soldier; <-- устаревшая форма
 
+// // Object.setPrototypeOf(john, soldier);
 
-// Urok 26 dinamich tipizaciya
-//1
-console.log(String(null));
-//2
-console.log(typeof(5 + ''));
-
-// To number
-console.log(typeof(Number('4')));
-console.log(typeof(+'5'));
-console.log(typeof(parseInt("15px", 10)));
-
-// To boolean
-
-// 0, '', null, undefined, NaN; // <- false
+// // console.log(john.armor);
+// john.sayHello();
 
 
+// // Urok 26 dinamich tipizaciya
+// //1
+// console.log(String(null));
+// //2
+// console.log(typeof (5 + ''));
+
+// // To number
+// console.log(typeof (Number('4')));
+// console.log(typeof (+'5'));
+// console.log(typeof (parseInt("15px", 10)));
+
+// // To boolean
+
+// // 0, '', null, undefined, NaN; // <- false
+
+// // YPOK 34
+// //touchstart
+// //touchmove
+// //touchend
+// //touchenter
+// //touchleave
+// //touchcancel
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+//         console.log('start');
+//         console.log(e.targetTouches);
+//     });
+
+//     box.addEventListener('touchmove', (e) => {
+//         e.preventDefault();
+//         console.log(e.targetTouches[0].pageX);
+//     });
+
+//     // box.addEventListener('touchend', (e) => {
+//     //     e.preventDefault();
+//     //     console.log('END');
+//     // });
+
+// });
+
+// // touches
+// // targetTouches
+// // changedTouches
+
+// YPOK 028
+
+// const box = document.getElementById('box'),
+//     btns = document.getElementsByTagName('button'), // psevdomassiv
+//     circles = document.getElementsByClassName('circle'), // HTML collection
+//     wrapper = document.querySelector('.wrapper'),
+//     hearts = wrapper.querySelectorAll('.heart'),
+//     oneHeart = wrapper.querySelector('.heart');
+
+// // console.dir(box);
+// // box.style.backgroundColor = 'brown';
+// // box.style.width = '500px';
+
+// box.style.cssText = 'background-color: pink; width: 600px';
+
+// btns[1].style.borderRadius = '100%';
+// circles[2].style.backgroundColor = 'red';
+
+// // for (let i = 0; i < hearts.length; i++) {
+// //     hearts[i].style.backgroundColor = 'green';
+// // }
+
+// hearts.forEach(item => {
+//     item.style.backgroundColor = 'purple';
+// });
+
+// // YPOK 29
+
+// const div = document.createElement('div');
+// // const text = document.createTextNode('Тут был я');
+
+// div.classList.add('black');
+// wrapper.append(div); // добавить элемент  в блок wrapper в конец
+// // wrapper.appendChild(div);
+
+// // wrapper.prepend(div); // добавить элемент  в блок wrapper в начало
+
+// // hearts[0].after(div); // добавить после элемента 
+// // hearts[0].before(div); // добавить до элемента
+
+// // wrapper.insertBefore(div, hearts[0]);
+
+// // circles[0].remove(); // удалить элемент
+// // wrapper.removeChild(hearts[1]);
+
+// // hearts[2].replaceWith(circles[0]);
+
+// // wrapper.replaceChild(circles[0], hearts[1]);
+// div.style.width = '300px';
+
+// div.innerHTML = '<h3>SUKA<h3>'
+
+// // div.textContent = '<h3>Hello WORLD!</h3>'; // не работает с html тегами
+
+// div.insertAdjacentHTML('afterend', '<h2> Allabai ne plach</h2>');
 
 
+// YPOK 31
+// const btns = document.querySelectorAll('button'),
+//     overlay = document.querySelector('.overlay');
+
+// // btn.onclick = function () {
+// //     alert('gaylord');
+// // }
+
+// // let i = 0;
+// const deleteElement = (event) => {
+//     console.log(event.currentTarget);
+//     console.log(event.type);
+//     // i++;
+//     // if (i == 1) {
+//     //     btn.removeEventListener('click', deleteElement);
+//     // }
+// }
+
+// // btn.addEventListener('click', deleteElement);
+// // overlay.addEventListener('click', deleteElement);
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', deleteElement, {
+//         once: true
+//     });
+// })
+
+// const link = document.querySelector('a');
+// link.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     console.log(event.target);
+// })
+
+
+// YPOK 32
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// for (let node of document.body.childNodes) {
+//     if (node.nodeName == '#text') {
+//         continue;
+//     }
+
+//     console.log(node);
+// }
+
+// // YPOK 35
+
+// const p = document.querySelectorAll('p');
+// console.log(p);
+
+
+
+// function loadScript(src) {
+//     const script = document.createElement('script');
+//     script.src = src;
+//     script.async = false;
+//     document.body.append(script);
+// }
+// loadScript('test.js');
+
+// YPOK 37 Classlist
+
+// const btns = document.querySelectorAll('button'),
+//     wrapper = document.querySelector('.btn-block');
+
+// // console.log(btns[0].classList.length);
+// // console.log(btns);
+// // console.log(btns[0].classList.item(1));
+// // console.log(btns[1].classList.add('red', 'allah'));
+// // // console.log(btns[0].classList.remove('blue'));
+// // console.log(btns[0].classList.toggle('blue'));
+
+// // if (btns[1].classList.contains('red')) {
+// //     console.log('red est');
+// // }
+// btns[0].addEventListener('click', () => {
+//     if (!btns[1].classList.contains('red')) {
+//         btns[1].classList.add('red');
+//     } else {
+//         btns[1].classList.remove('red');
+//     }
+// });
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.matches("button")) {
+//         console.log('HELLO');
+//     }
+// });
+
+// // btns.forEach(elem => {
+// //     elem.addEventListener('click', () => {
+// //         console.log('HELLO');
+// //     });
+
+// // });
+
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
+
+// YPOK 39
+
+// const btn = document.querySelector('.btn');
+// let timerId,
+//     i = 0;
+
+
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
+//     const id = setInterval(frame, 10);
+
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimation);
+
+// function logger() {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+
+// }
+
+// let id = setTimeout(function log() {
+//     console.log("hello");
+//     id = setTimeout(log, 500);
+// }, 500);
+
+// YPOK 40
+
+const now = new Date('2022-02-14');
+
+// new Date.parse('2022-02-14');
+
+console.log(now.setHours(40)); // Year, month и прочее...
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate()); // Get hours, minutes, seconds...
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+
+console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+console.log(end - start);
